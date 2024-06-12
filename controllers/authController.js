e15808b00ae410862c84d5912fc9663bae874643
Morgan <160355750+SirMeOWski22@users.noreply.github.com>
2024-06-11 23:02:52 -0500

    Added recipe and user controllers. Added  dependencies.

---
+++ controllers/authController.js
@@ -0,0 +1,29 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+
+exports.register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newuser = await User.create({ username, email, password: hashedPassword });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ eror: 'Failed to register user' });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+      res.json({ token });
+    } else {
+      res.status(401).json({ error: 'Invalid email or password' });
+    }
+  } catch (error) {
+    res.status(500).json({ eror: 'Failed to login' });
+  }
+};
