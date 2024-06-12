f37020dc9e2e0d4113079ea2ac6cdb29fd8e028f
Morgan <160355750+SirMeOWski22@users.noreply.github.com>
2024-06-11 22:36:34 -0500

    Fixed typos in code.

--- routes/api/recipeRoutes.js
+++ routes/api/recipeRoutes.js
@@ -4,9 +4,9 @@ const recipeController = require('../../controllers/recipeController');
 
 //Recipe-related Routes
 router.post('/', recipeController.createRecipe);
-router.get('/', recipeControllerController.getAllRecipes);
-router.get('/:id', recipeControllerController.getRecipeById);
-router.get('/:id', recipeControllerController.updateRecipe);
-router.get('/:id', recipeControllerController.deleteRecipe);
+router.get('/', recipeController.getAllRecipes);
+router.get('/:id', recipeController.getRecipeById);
+router.put('/:id', recipeController.updateRecipe);
+router.delete('/:id', recipeController.deleteRecipe);
 
 module.exports = router;
