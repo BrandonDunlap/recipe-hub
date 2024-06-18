f6959ebb3ceaf924aa74df50330ca16204cb075d
Morgan <160355750+SirMeOWski22@users.noreply.github.com>
2024-06-17 21:46:00 -0500

    connect to postgresql

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,10 @@
-module.exports = {
-  HOST: process.env.DB_HOST,
-  USER: process.env.DB_USER,
-  PASSWORD: process.env.DB_PASSWORD,
-  DB: process.env.DB_NAME,
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-};
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'postgres',
+  });
+}
